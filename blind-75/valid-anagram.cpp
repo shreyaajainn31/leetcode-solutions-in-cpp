5d5c1109bcf11e0d855341348feea862a16314ae
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-23 23:37:53 -0400

    #242

---
+++ blind-75/valid-anagram.cpp
@@ -0,0 +1,42 @@
+/*
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+https://leetcode.com/problems/valid-anagram/
+
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        return s == t;
+        
+    }
+};
+
+int main(){
+  Solution obj;
+  cout<<obj.isAnagram("eat", "ate");
+}
