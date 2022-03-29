52e9727aeab4652d98015b85b2e5fcbfefc6b384
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-03-29 11:52:29 -0400

    #230

---
+++ blind-75/kth-smallest-element-in-bst.cpp
@@ -0,0 +1,58 @@
+/*
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+230. Kth Smallest Element in a BST
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int answer = 0;
+    
+    void traverse(vector<int>&inorder,TreeNode *root, int k){
+       if(root == NULL){
+           return;
+       }
+        
+       traverse(inorder,root->left, k);
+       inorder.push_back(root->val);
+       traverse(inorder, root->right,k);
+        
+    }
+    
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>inorder;
+        traverse(inorder,root,k);
+        return inorder[k-1];
+        
+    }
+};
