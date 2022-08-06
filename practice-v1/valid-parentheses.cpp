8437c2eadaa148bab36f1a4fe5a0b0b2783d9f73
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-08-05 20:43:11 -0700

    #20

---
+++ practice-v1/valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        
+        for(char c : s){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }
+            else {
+                if(stack.empty()){
+                    return false;
+                }
+                else if( c== ')') {
+                    if( stack.top() != '('){
+                        return false;
+                    }
+                }
+                else if( c== ']') {
+                    if( stack.top() != '['){
+                        return false;
+                    }
+                }
+                else if( c== '}') {
+                    if( stack.top() != '{'){
+                        return false;
+                    }
+                }
+                
+                stack.pop();
+            }
+        }
+        
+        return stack.empty();
+    }
+};
